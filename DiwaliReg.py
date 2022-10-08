cdd2852f777d6cd321cc80836df43873f81bc18d
Praveen SIDDU <praveensiddu@users.noreply.github.com>
2022-10-08 00:03:47 -0400

    parameterize env values

--- DiwaliReg.py
+++ DiwaliReg.py
@@ -1,16 +1,34 @@
 import pandas as pd
 import gspread
+from dotenv import load_dotenv
+import os
 from df2gspread import df2gspread as d2g
 from oauth2client.service_account import ServiceAccountCredentials
+
+load_dotenv()
+credentials_json = os.getenv("CREDENTIALS_JSON", 'account_credentials.json')
+
+sheet_id = os.getenv("SHEET_ID")
+if sheet_id is None:
+    print('SHEET_ID env variable is mandatory')
+
+target_sheet_id = os.getenv("TARGET_SHEET_ID")
+if target_sheet_id is None:
+    print('TARGET_SHEET_ID env variable is mandatory')
+
+worksheet = os.getenv("WORKSHEET_NAME")
+if worksheet is None:
+    print('WORKSHEET_NAME env variable is mandatory')
+
+
 
 # Set up credentials
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('utsav.json', scopes = scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_name(credentials_json, scopes = scope)
 gc = gspread.authorize(credentials)
 
 # Pull data from original google sheet
-sheet_id = "1fWuFKySxg08LYTT7-xBKAY6YuU12JJW3S7CNqrWIKqE"
 sheet_name = "FormResponses"
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 formResponses = pd.read_csv(url)
@@ -20,17 +38,16 @@ formResponses = pd.read_csv(url)
 extractedResponse = formResponses.iloc[:, [1, 2, 3, 4, 5, 7, 8, 9, 10]]
 
 # Push to another google sheet
-newSheetId = '1BRNKkIXHAJTkhk_XA4q_biKQSEfWT_w4c2fpcbPYG3M'
-worksheet = 'EventInfo'
+
 
 # Save the original Notes and add them back into the dataframe
-newUrl = f"https://docs.google.com/spreadsheets/d/{newSheetId}/gviz/tq?tqx=out:csv&sheet={worksheet}"
+newUrl = f"https://docs.google.com/spreadsheets/d/{target_sheet_id}/gviz/tq?tqx=out:csv&sheet={worksheet}"
 reducedResponse = pd.read_csv(newUrl)
 extractedResponse['Notes'] = reducedResponse['Notes']
 # Replace nan with empty string
 extractedResponse = extractedResponse.fillna('')
 
 # Push to new google sheet
-d2g.upload(extractedResponse, newSheetId, worksheet, credentials=credentials, row_names=False)
+d2g.upload(extractedResponse, target_sheet_id, worksheet, credentials=credentials, row_names=False)
 
 pass
--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Gsheet processing tool
 Goal of this utility is to 
-1. read a gsheet
-2. copy selected columns to another gsheet 
+1. read a gsheet(source)
+2. copy selected columns to another gsheet(target) 
 3. if there are more columns in another gsheet those should remain untouched.
 
 The first sheet is the master sheet which contains all information. Only some information is relevant for human users. That is extracted into a second sheet. Human users can then add some more notes(columns) into the second sheet. Those notes should not be overwritten if we rerun this tool. If there are new rows in the first sheet when we rerun this tool it should be copied to the second sheet.
+
+## Implementation details
+1. create both source sheet and target sheet
+2. get sheet id and create a .env file with it
+3. 
